dfd90249a7d7dd1d103b1f930df06025c2b1c3e2
maxwhalezhou <nerdtothemax@gmail.com>
2016-11-29 23:18:36 -0800

    Working NavBar with Router

--- src/NewPost.js
+++ src/NewPost.js
@@ -3,14 +3,17 @@ import { hashHistory, Link } from "react-router";
 import firebase from "firebase";
 
 class NewPost extends React.Component {
-     constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = { post: '', loading: false };
+    this.state = { post: '', title: '', loading: false };
   }
 
   //when the text in the form changes
   updatePost(event) {
     this.setState({ post: event.target.value });
+  }
+  updateTitle(event) {
+    this.setState({ title: event.target.value });
   }
 
   //post a new chat to the database
@@ -24,11 +27,11 @@ class NewPost extends React.Component {
       text: this.state.post,
       userId: firebase.auth().currentUser.uid, //to look up user info
       time: firebase.database.ServerValue.TIMESTAMP, //MAGIC
-      comments: {}
+      title: this.state.title
     };
     postsRef.push(newPost).then((response) => { this.setState({ loading: false }) }); //upload
 
-    this.setState({ post: '' }); //empty out post (controlled input)
+    this.setState({ post: '', title: '' }); //empty out post (controlled input)
   }
   savePost(event) {
     event.preventDefault(); //don't submit
@@ -40,26 +43,24 @@ class NewPost extends React.Component {
       text: this.state.post,
       userId: firebase.auth().currentUser.uid, //to look up user info
       time: firebase.database.ServerValue.TIMESTAMP, //MAGIC
+      title: this.state.title
     };
     postsRef.push(newPost).then((response) => { this.setState({ loading: false }) }); //upload
 
-    this.setState({ post: '' }); //empty out post (controlled input)
+    this.setState({ post: '', title: '' }); //empty out post (controlled input)
   }
 
   // how to display
   render() {
-     firebase.auth().signInWithEmailAndPassword("maxwell.chi.zhou@gmail.com", "password")
-      .catch(err => this.setState({'error':true}));  
     var currentUser = firebase.auth().currentUser; //get the curent user
-    console.log("current user is: " + currentUser);
-//     console.log("currentuser displayName is: " + currentUser.handle);
     return (
       <div className="message-box write-message" role="region">
         {this.state.loading &&
           <p className="loading">Uploading...</p>
         }
         <form className="message-input" role="form">
-          <textarea placeholder="Type post here..." name="text" value={this.state.post} className="post-form form-control" onChange={(e) => this.updatePost(e)}></textarea>
+          <textarea placeholder="Type title here..." name="text" className="post-form form-control" onChange={(e) => this.updateTitle(e)}></textarea>
+          <textarea placeholder="Type post here..." name="text" className="post-form form-control" onChange={(e) => this.updatePost(e)}></textarea>
           <div className="form-group send-message">
             {/* Disable if invalid post length */}
             <button className="btn btn-primary" disabled={this.state.post.length === 0 || this.state.loading}
@@ -68,10 +69,10 @@ class NewPost extends React.Component {
               onClick={(e) => this.savePost(e)} >
               <i className="fa fa-pencil-square-o" aria-hidden="true"></i> Save
             </button>
-            </div>
+          </div>
         </form>
       </div>
     );
-    }
+  }
 }
 export default NewPost;
