e0b52ab67669464c0a1710ee8b7f8e6b22b5c260
Nancy Tran <nhvt@uw.edu>
2016-11-30 03:59:36 -0800

    Add edit modal

--- src/Saved.js
+++ src/Saved.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { hashHistory, Link } from "react-router";
+import { Modal, Button } from "react-bootstrap";
 import firebase from "firebase";
 
 class Saved extends React.Component {
@@ -39,7 +40,7 @@ class PostList extends React.Component {
 
         savedPostsRef.on("value", (snapshot) => {
             var savedPostsArray = [];
-            snapshot.forEach(function (child) {
+            snapshot.forEach(function(child) {
                 var post = child.val();
                 post.key = child.key;
                 savedPostsArray.push(post);
@@ -62,17 +63,57 @@ class PostList extends React.Component {
 }
 
 class PostItem extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { editShow: false };
+    }
+
     render() {
         var text = this.props.post.text;
         if (this.props.post.text.length > 200) {
             var text = text.substring(0, 200) + "...";
         }
-        
+
+        let editClose = () => this.setState({ editShow: false });
+
         return (
             <div>
                 <h3>{this.props.post.title}</h3>
                 <div>{text}</div>
+                <Button onClick={() => this.setState({ editShow: true })}>Edit</Button>
+
+                <EditModal post={this.props.post} show={this.state.editShow} onHide={editClose} />
             </div>
+        );
+    }
+}
+
+class EditModal extends React.Component {
+    editPost(post) {
+        console.log("Post has been edited!");
+    }
+
+    postPost(post) {
+        console.log("Post has been published!");
+    }
+
+    render() {
+        return (
+            <Modal {...this.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-lg">
+                        <textarea defaultValue={this.props.post.title} />
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <textarea defaultValue={this.props.post.text} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => this.editPost(this.props.post)}>Edit</Button>
+                    <Button bsStyle="primary" onClick={() => this.postPost(this.props.post)}>Post</Button>
+                </Modal.Footer>
+            </Modal>
         );
     }
 }
