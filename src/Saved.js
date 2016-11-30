f6f51593820ef50ed453f0cd188b68c9dc1a0ef0
Nancy Tran <nhvt@uw.edu>
2016-11-30 02:02:49 -0800

    Add saved post feature

--- src/Saved.js
+++ src/Saved.js
@@ -4,7 +4,74 @@ import firebase from "firebase";
 
 class Saved extends React.Component {
     render() {
-        return(<div>Saved</div>);
+        return (
+            <div>
+                <h2>Saved Posts</h2>
+                <div>
+                    <PostList />
+                </div>
+            </div>
+        );
+    }
+}
+
+class PostList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { savedPosts: [] }
+    }
+
+    componentDidMount() {
+        var unregister = firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                console.log('Auth state changed: logged in as', user.email);
+                this.getPosts();
+            } else {
+                unregister();
+                console.log('Auth state changed: logged out');
+                hashHistory.push('/login');
+            }
+        });
+    }
+
+    getPosts() {
+        var savedPostsRef = firebase.database().ref("Users/" + firebase.auth().currentUser.uid + "/saved");
+
+        savedPostsRef.on("value", (snapshot) => {
+            var savedPostsArray = [];
+            snapshot.forEach(function (child) {
+                savedPostsArray.push(child.val());
+            });
+            this.setState({ savedPosts: savedPostsArray });
+        });
+    }
+
+    render() {
+        var posts = this.state.savedPosts.map((post) => {
+            return <PostItem post={post} key={post.key} />
+        });
+
+        return (
+            <div>
+                {posts}
+            </div>
+        );
+    }
+}
+
+class PostItem extends React.Component {
+    render() {
+        var text = this.props.post.text;
+        if (this.props.post.text.length > 200) {
+            var text = text.substring(0, 200) + "...";
+        }
+        
+        return (
+            <div>
+                <h3>{this.props.post.title}</h3>
+                <div>{text}</div>
+            </div>
+        );
     }
 }
 
