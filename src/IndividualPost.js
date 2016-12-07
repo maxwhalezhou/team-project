ffae52e64aa272677697a10f3e0bb1fb3e10336b
Arden <kmk3170@uw.edu>
2016-12-07 10:54:33 -0800

    comments and accessibility

--- src/IndividualPost.js
+++ src/IndividualPost.js
@@ -4,11 +4,13 @@ import { hashHistory } from "react-router";
 import firebase from "firebase";
 import Time from "react-time";
 
+// This component shows a post title, content, writer info, and comments 
 class IndividualPost extends React.Component {
     constructor(props) {
         super(props);
         this.state = { post: undefined };
     }
+    
     componentDidMount() {
         var unregister = firebase.auth().onAuthStateChanged(user => {
             if (!user) {
@@ -18,20 +20,18 @@ class IndividualPost extends React.Component {
         });
         this.searchPosts(this.props.params);
     }
+
+    // searches for the post user clicked in to find ref 
     searchPosts(param) {
-        //only executes if there is a channel param
-        //getting the last 100 posts
         var postKey = param.post;
         var postsRef = firebase.database().ref("Users");
         this.PostsRef = postsRef;
         var thisPost = "placeholder";
         postsRef.on('value', (snapshot) => {
             //going through posts and pushing the value into array
-
             snapshot.forEach(function (child) {
                 if (child.val().published) {
                     var post = child.val().published[postKey];
-                    // post.key = postKey; //save the unique id for later
                     if (post) {
                         thisPost = post;
                     }
@@ -40,26 +40,29 @@ class IndividualPost extends React.Component {
             this.setState({ post: thisPost });
         });
     }
+
     componentWillUnmount = () => {
         this.PostsRef.off();
     }
+
+    // how it displays
     render() {
         return (
-            <div>
+            <div role="region">
                 {!this.state.post &&
-                    <h3>loading</h3>
+                    <h3 role="banner">loading...</h3>
                 }
                 {this.state.post &&
-                    <div>
-                        <div>
-                            <h2>{this.state.post.title}</h2>
-                            <p>{this.state.post.handle} at <Time value={this.state.post.time} relative /></p>
-                            <p className="white-space">{this.state.post.text}</p>
+                    <div role="region" aria-label="Post">
+                        <div aria-label="Writer Info">
+                            <h2 role="banner" aria-label="Post Title">{this.state.post.title}</h2>
+                            <p aria-label="Author Handle">{this.state.post.handle} at <Time value={this.state.post.time} relative /></p>
+                            <p className="white-space" aria-label="Content">{this.state.post.text}</p>
                         </div>
-                        <div className="comments-box">
+                        <div className="comments-box" role="region" aria-label="Comments">
                             <label>Leave A Comment</label>
-                            <PostForm post={this.props.params.post} writer={this.state.post.userId} />
-                            <div className="comments">
+                            <PostForm role="form" post={this.props.params.post} writer={this.state.post.userId} />
+                            <div className="comments" role="region">
                                 <CommentList post={this.props.params.post} writer={this.state.post.userId} />
                             </div>
                         </div>
@@ -71,25 +74,29 @@ class IndividualPost extends React.Component {
 
 }
 
+// A component that displays post form for comment 
 class PostForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = { comment: '', loading: false }
     }
+
+    // updates current state with new comment
     updateComment(event) {
         this.setState({ comment: event.target.value });
     }
 
+    // posts a new comment
     postComment(event) {
         event.preventDefault(); //don't submit
-        this.setState({ loading: true }); //empty out post (controlled input)
+        this.setState({ loading: true });
 
         /* Add a new Chat to the database */
-        var commentsRef = firebase.database().ref('Users/' + this.props.writer + '/published/' + this.props.post + '/comments'); //the chats in the channel
+        var commentsRef = firebase.database().ref('Users/' + this.props.writer + '/published/' + this.props.post + '/comments'); //the comments in the post
         var newComment = {
             text: this.state.comment,
             userId: firebase.auth().currentUser.uid, //to look up user info
-            handle: firebase.auth().currentUser.displayName, //to look up user info
+            handle: firebase.auth().currentUser.displayName, // user handle
             time: firebase.database.ServerValue.TIMESTAMP, //MAGIC
         };
         commentsRef.push(newComment).then((response) => { this.setState({ loading: false }) }); //upload
@@ -97,22 +104,23 @@ class PostForm extends React.Component {
         this.setState({ comment: '' }); //empty out post (controlled input)
     }
 
+    // how to display
     render() {
         return (
             <div>
                 {this.state.loading &&
                     <p className="loading">Uploading...</p>
                 }
-                <form className="form-group">
+                <form role="role" aria-label="Comment Form" className="form-group">
                     <textarea type="text" placeholder="Type comment here..." value={this.state.comment} className="form-control" onChange={(e) => this.updateComment(e)} />
-                    <Button className="btn btn-primary btn-block" disabled={this.state.comment.length === 0 } onClick={(e) => this.postComment(e)} >Post</Button>
+                    <Button aria-label="Post" className="btn btn-primary btn-block" disabled={this.state.comment.length === 0 } onClick={(e) => this.postComment(e)} >Post</Button>
                 </form>
             </div>
         );
     }
 }
 
-
+// This component shows a list of comments on a post
 class CommentList extends React.Component {
     constructor(props) {
         super(props);
@@ -128,10 +136,12 @@ class CommentList extends React.Component {
 
         this.getComments();
     }
+
     componentWillReceiveProps() {
         this.getComments();
     }
 
+    // gets all comments from the selected post
     getComments() {
         var commentsRef = firebase.database().ref('Users/' + this.props.writer + '/published/' + this.props.post + '/comments'); //the chats in the channel
         commentsRef.on('value', (snapshot) => {
@@ -146,8 +156,9 @@ class CommentList extends React.Component {
         });
     }
 
+    // how to display
     render() {
-        //don't show if don't have user data yet (to avoid partial loads)
+        //don't show if don't have comment data yet (to avoid partial loads)
         if (!this.state.comments) {
             return null;
         }
@@ -166,23 +177,26 @@ class CommentList extends React.Component {
         }
 
         return (
-            <div>
+            <div role="region">
                 <p>{commentItems.length} {commentDisplay}</p>
                 {commentItems}
             </div>);
     }
 }
 
+// This component shows a comment, allows users to edit, delete their comment
 class CommentItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = { editShow: false, deleteShow: false, text: this.props.comment.text, edited: false };
     }
 
+    // updates the current state with new edit
     updateText(event) {
         this.setState({ text: event.target.value });
     }
 
+    // updates the comment with new edited comment
     editPost() {
         var commentRef = firebase.database().ref("Users/" + this.props.postWriter + "/published/" + this.props.post + "/comments/" + this.props.comment.key);
         commentRef.child("text").set(this.state.text);
@@ -193,11 +207,13 @@ class CommentItem extends React.Component {
         }, 1500);
     }
 
+    // deletes a comment
     deleteComment() {
         var commentRef = firebase.database().ref("Users/" + this.props.postWriter + "/published/" + this.props.post + "/comments/" + this.props.comment.key);
         commentRef.remove();
     }
 
+    // how to display
     render() {
         let editClose = () => this.setState({ editShow: false });
         let deleteClose = () => this.setState({ deleteShow: false });
@@ -219,8 +235,8 @@ class CommentItem extends React.Component {
                 </div>
                 {this.props.comment.userId === firebase.auth().currentUser.uid &&
                     <div className="panel-footer">
-                        <Button bsSize="small" onClick={() => this.setState({ editShow: true })}>Edit</Button>
-                        <Button bsStyle="danger" bsSize="small" onClick={() => this.setState({ deleteShow: true })}>Delete</Button>
+                        <Button aria-label="Edit" bsSize="small" onClick={() => this.setState({ editShow: true })}>Edit</Button>
+                        <Button aria-label="Delete" bsStyle="danger" bsSize="small" onClick={() => this.setState({ deleteShow: true })}>Delete</Button>
                     </div>
                 }
 
@@ -239,8 +255,8 @@ class CommentItem extends React.Component {
                         }
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button bsStyle="primary" onClick={() => this.editPost()}>Save</Button>
-                        <Button onClick={editClose}>Cancel</Button>
+                        <Button aria-label="Save" bsStyle="primary" onClick={() => this.editPost()}>Save</Button>
+                        <Button aria-label="Cancel" onClick={editClose}>Cancel</Button>
                     </Modal.Footer>
                 </Modal>
 
@@ -254,8 +270,8 @@ class CommentItem extends React.Component {
                         This will permanently delete your comment.
                 </Modal.Body>
                     <Modal.Footer>
-                        <Button bsStyle="danger" onClick={() => this.deleteComment()}>Yes</Button>
-                        <Button onClick={deleteClose}>No</Button>
+                        <Button aria-label="Delete" bsStyle="danger" onClick={() => this.deleteComment()}>Yes</Button>
+                        <Button aria-label="Cancel" onClick={deleteClose}>No</Button>
                     </Modal.Footer>
                 </Modal>
             </div>
